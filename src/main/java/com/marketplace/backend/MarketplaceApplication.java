34a3a8eff14f30b8cbc519fc11c0a312a43d32a6
Mosneag Gheorghe <m.gelu99@gmail.com>
2021-11-11 16:40:08 +0200

    Config Spring Cloud

--- src/main/java/com/marketplace/backend/MarketplaceApplication.java
+++ src/main/java/com/marketplace/backend/MarketplaceApplication.java
@@ -3,9 +3,11 @@ package com.marketplace.backend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @MapperScan("com.marketplace.backend.mapper")
+//@EnableConfigServer
 public class MarketplaceApplication {
 
 	public static void main(String[] args) {
